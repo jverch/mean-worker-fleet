7a2281939eb1a4500888f72ba0a99b11ea4e03d2
James <verchjames@gmail.com>
2023-08-23 17:37:01 -0700

    Rudimentary data generator

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -5,6 +5,8 @@ interface HomeProps {}
 
 const Home: FC<HomeProps> = () => {
   const [data, setData] = React.useState(null);
+  const [fileCount, setFileCount] = React.useState('1');
+  const [numberCount, setNumberCount] = React.useState('1');
 
   React.useEffect(() => {
     fetch("/api")
@@ -12,10 +14,57 @@ const Home: FC<HomeProps> = () => {
       .then((data) => setData(data.message));
   }, []);
 
+  // generate function
+  const generate = (fileCount: string, numberCount: string) => {
+    console.log("generating?");
+    let currFileCount = parseInt(fileCount);
+    let currNumberCount = parseInt(numberCount);
+    if (isNaN(currFileCount) || isNaN(currNumberCount)) {
+      return alert("File Count and Number Count must be numbers");
+    };
+    if (currFileCount < 1 || currFileCount > 100 || currNumberCount < 1 || currNumberCount > 1000000) {
+      return alert("File Count must be a number within the range of 1-100 and Number Count within the range of 1-1000000");
+    };
+    fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fileCount, numberCount }),
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="Home" data-testid="Home">
       Home Component
       <p>{!data ? "Loading..." : data}</p>
+      <div>
+        <form>
+          <h1>Generate Files:</h1>
+          <label>
+            File Count: 
+            <input
+              onChange={(e) => setFileCount(e.target.value)}
+              value={fileCount}
+              type="text"
+              name="fileCount" 
+            />
+          </label>
+          <label>
+            Number Count: 
+            <input
+              onChange={(e) => setNumberCount(e.target.value)}
+              value={numberCount}
+              type="text"
+              name="numberCount"
+            />
+          </label>
+        </form>
+        <button onClick={() => generate(fileCount, numberCount)}>Generate</button>
+      </div>
     </div>
   )
 };
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { GenerateFiles } from './lib/utils';
 
 dotenv.config();
 
 const app: Express = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
+
+const jsonParser = bodyParser.json();
+app.use(errorHandler);
 
 app.get('/api', (req: Request, res: Response) => {
   console.log('GET /api');
@@ -16,6 +21,34 @@ app.get('/', (req: Request, res: Response) => {
   res.send({message: 'Hello from Express Server! (/ endpoint)'});
 });
 
+app.post('/api/generate', jsonParser, (req: Request, res: Response) => {
+  console.log('POST /api/generate');
+  console.log(req.body);
+  let fileCount = req.body?.fileCount;
+  let numberCount = req.body?.numberCount;
+  if (req.body && fileCount && numberCount) {
+    GenerateFiles(fileCount, numberCount);
+    res.send({message: 'Generating files!'});
+  } else {
+    res.status(400).send({message: 'Invalid request body'});
+  }
+});
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+function errorHandler(error: Error, req: Request, res: Response, next: Function) {
+  console.log(error);
+  res.status(500).send({message: 'Internal server error'});
+}
+
+// For future reference:
+// const fs = require("fs");
+// fs.createReadStream("data.csv", { encoding: "utf-8" })
+//   .on("data", (chunk) => {
+//     console.log(chunk);
+//   })
+//   .on("error", (error) => {
+//     console.log(error);
+//   });
